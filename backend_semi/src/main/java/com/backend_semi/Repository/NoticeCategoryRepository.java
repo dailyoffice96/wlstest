61482dc3d2466215ded5d5fae74429007cba7426
dailyoffice96 <dailyoffice96@gmail.com>
2026-06-05 10:59:37 +0900

    내용 수정

--- backend_semi/src/main/java/com/backend_semi/Repository/NoticeCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.backend_semi.repository;
-
-import com.backend_semi.entity.NoticeCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeCategoryRepository extends  JpaRepository<NoticeCategory, Long> {
-}
