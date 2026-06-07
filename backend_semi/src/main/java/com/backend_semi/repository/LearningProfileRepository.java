6eb1777e00e115596f48ee28c26bb24d07674277
INS <dlsdlek21@naver.com>
2026-06-07 12:34:45 +0900

    Bug fix

--- backend_semi/src/main/java/com/backend_semi/repository/LearningProfileRepository.java
+++ backend_semi/src/main/java/com/backend_semi/repository/LearningProfileRepository.java
@@ -3,7 +3,5 @@ package com.backend_semi.repository;
 import com.backend_semi.entity.LearningProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface LearningProfileRepository extends JpaRepository<LearningProfile, Long> {
 }
